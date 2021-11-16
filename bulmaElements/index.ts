cefbe071b19e6636bba30ae6b1762e1f20582796
huequica <dev@huequica.xyz>
2021-11-16 23:03:53 +0900

    入れた

--- bulmaElements/index.ts
+++ bulmaElements/index.ts
@@ -1,4 +1,5 @@
 import * as Elements from './elements/elements';
+import * as Layout from './layout/layout';
 import Column from './column';
 
-export { Elements, Column };
+export { Elements, Layout, Column };
