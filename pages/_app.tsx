461aa087d2642bd1b42bb21a9d21da20032a65a8
huequica <dev@huequica.xyz>
2021-11-18 23:15:29 +0900

    SCSS を移植

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import 'scss/base.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />;
